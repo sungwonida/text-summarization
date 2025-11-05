20873cf60cc9619729b3241fa6b5467bd185ee19
David Jung <sungwonida@gmail.com>
2025-11-05 23:26:51 +0900

    Add CLI invocation logging and ignore artifacts

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -13,7 +13,7 @@ from .inspection import (
     collect_samples_for_inspection,
     save_inspection_artifacts,
 )
-from .logging_utils import configure_logging, tensorboard_writer_context
+from .logging_utils import configure_logging, record_cli_invocation, tensorboard_writer_context
 from .runtime import get_device, seed_everything
 from .training import build_compute_metrics, build_training_arguments, create_trainer
 
@@ -36,5 +36,6 @@ __all__ = [
     "parse_training_args",
     "preprocess_function",
     "save_inspection_artifacts",
+    "record_cli_invocation",
     "tensorboard_writer_context",
 ]
